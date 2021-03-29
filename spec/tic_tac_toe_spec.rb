d86cf8098ebec5626278220236a2038e89a04976
Elmer Lopez <meme_lopez10@hotmail.com>
2021-03-29 15:26:23 -0600

    examples for board

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -1,1 +1,23 @@
+#spec/tic_tac_toe_spec.rb
+
+
 require './lib/tic_tac_toe.rb'
+
+
+describe Board do
+  describe ".squares" do
+    it "returns a hash that respresents the board" do
+      expect(Board.squares).to eql( {1 => '-', 2 => '-', 3 => '-', 4 => '-', 5 => '-', 6 => '-', 7 => '-', 8 => '-', 9 => '-'} )
+    end
+  end
+  
+  describe ".reset" do
+    it "returns a hash that respresents the board" do
+      expect(Board.squares).to eql( {1 => '-', 2 => '-', 3 => '-', 4 => '-', 5 => '-', 6 => '-', 7 => '-', 8 => '-', 9 => '-'} )
+    end
+  end
+end
+
+describe Player do
+  
+end
